9bec82afea894ac6abb8b4b4fee2138fb438333f
MochiCoder2043 <ericpuente2009@gmail.com>
2025-11-18 09:30:56 -0600

    added full functionality to the pelvis

--- opmodes/Jack.java
+++ opmodes/Jack.java
@@ -2,17 +2,24 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.hardware.DriveTrain;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Jack1", group="Iterative OpMode")
+import org.firstinspires.ftc.teamcode.hardware.DriveTrain;
+import org.firstinspires.ftc.teamcode.hardware.Pelvis;
+
+@TeleOp(name="Jack", group="Iterative OpMode")
 public class Jack extends OpMode {
 
     private DriveTrain drive = new DriveTrain();
+    private Pelvis pelvis = new Pelvis();
+
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void init() {
 
         drive.init(hardwareMap);
+        pelvis.init(hardwareMap);
         telemetry.addLine("DriveTrain initialized");
 
     }
@@ -26,13 +33,21 @@ public class Jack extends OpMode {
     @Override
     public void loop() {
 
+        // Gamepad inputs for the chasis movement
         double power = -gamepad1.left_stick_y;
         double strafe = gamepad1.right_trigger - gamepad1.left_trigger;
         double twist = gamepad1.right_stick_x;
 
-        drive.drive(power, strafe, twist);
+        // Gamepad inputs for throwing the ball
+        double pelvisInput = gamepad2.left_stick_y;
+        double intakePower = gamepad2.right_stick_y;
 
+        drive.drive(power, strafe, twist);
+        pelvis.launch(pelvisInput, intakePower);
+
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Drive", drive.getStatus());
+        telemetry.addData("Pelvis", pelvis.getStatus());
         telemetry.update();
 
     }
